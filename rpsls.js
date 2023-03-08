998804e09ccc9e5a42c1d32209fa54baabfbef81
Garrett Long WSL Ubuntu <garrettlong097@gmail.com>
2023-03-08 15:51:05 -0500

    first test

---
+++ rpsls.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+
+function beat(choice1, choice2){
+	switch(choice1){
+		case 'rock':
+			switch(choice2){
+				case 'paper':
+					return false
+				case 'scissors':
+					return true
+				case 'lizard':
+					return true
+				case 'spock':
+					return false
+			}
+		case 'paper':
+			switch(choice2){
+				case 'scissors':
+					return false
+				case 'lizard':
+					return false
+				case 'spock':
+					return true
+			}
+		case 'scissors':
+			switch(choice2){
+				case 'lizard':
+					return true
+				case 'spock':
+					return false
+			}
+		case 'lizard':
+			if(choice2 == 'spock') { return true }
+		default:
+			return !beat(choice2, choice1)
+	}
+}
+
+import {random, floor} from "mathjs"
+let shots = ['rock', 'paper', 'scissors']
+export function rps(shot){
+	var choice = shots[floor(random() * shots.length)]
+	if(shot === null || shot === undefined){
+		return {'player': choice}
+	}
+        if(shots.includes(shot)){
+		if(shot === choice){
+			var result = 'tie'
+		}
+		else{
+			var result = beat(shot, choice) ? 'win' : 'lose'
+		}
+                return {
+                        'player': shot,
+                        'opponent': choice,
+                        'result': result
+                }
+        }
+        else{
+                throw RangeError()
+        }
+}
+export function rpsls(shot){
+	shots.push('lizard')
+	shots.push('spock')
+	return rps(shot)
+}
---
+++ server.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2), {
+	int: ['port']
+})
+
+import express from 'express'
+import {rps, rpsls} from './rpsls.js'
+var app = express()
+
+app.get('/app/', function(req, res){
+	res.status = 200
+	res.send("200 OK")
+})
+
+app.get('/rps/', function(req, res){
+	res.status=200
+	res.send(rps())
+})
+
+app.get('/rpsls/', function(req, res){
+	res.status=200
+	res.send(rpsls())
+})
+
+app.get('/app/rps/play/:shot((paper)|(rock)|(scissors))', function(req, res){
+	res.status=200
+	res.send(rps(req.params.shot))
+})
+
+app.get('/app/rpsls/play/:shot((paper)|(rock)|(scissors)|(lizard)|(spock))', function(req, res){
+	res.status=200
+	res.send(rpsls(req.params.shot))
+})
+
+app.use(function(req, res, next) {
+	var error = new Error('Not Found')
+	error.status = 404;
+	res.send('404 Not Found')
+})
+
+args['port'] ??= 5000
+app.listen(args['port'])
