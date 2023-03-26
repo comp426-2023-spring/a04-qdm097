8f82ab85846e93acb4d27ba3db68a8b0a41c35c9
Garrett Long WSL Ubuntu <garrettlong097@gmail.com>
2023-03-26 15:16:09 -0400

    another test11!

--- server.js
+++ server.js
@@ -13,50 +13,63 @@ const app = express()
 app.use(express.json())
 //app.use(express.urlencoded({extended:true}))
 
+var i = 0
+
 app.get('/app', (req, res) => {
 	res.status(200).send("200 OK")
 })
+console.log(i); i++;
 
 app.get('/app/rps', (req, res) => {
 	res.status(200).send(JSON.stringify(rps()))
 })
+console.log(i); i++;
 
 app.get('/app/rpsls', (req, res) => {
 	res.status(200).send(JSON.stringify(rpsls()))
 })
+console.log(i); i++;
 
 // JSON params
 app.get('/app/rps/play', (req, res) => {
 	res.status(200).send(JSON.stringify(rps(req.query.shot)))
 })
+console.log(i); i++;
 
 app.get('/app/rpsls/play', (req, res) => {
 	res.status(200).send(JSON.stringify(rpsls(req.query.shot)))
 })
+console.log(i); i++;
 
 // query params
 app.get('/app/rps/play/:shot', (req, res) => {
 	res.status(200).send(JSON.stringify(rps(req.body.shot)))
 })
+console.log(i); i++;
 
 app.get('/app/rpsls/play/:shot', (req, res) => {
 	res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
 })
+console.log(i); i++;
 
 // params
 app.get('/app/rps/play/:shot', (req, res) => {
 	res.status(200).send(JSON.stringify(rps(req.params.shot)))
 })
+console.log(i); i++;
 
 app.get('/app/rpsls/play/:shot', (req, res) => {
 	res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
 })
+console.log(i); i++;
 
 
 app.get('*', (req, res) => {
 	res.status(404).send('404 NOT FOUND')
 })
+console.log(i); i++;
 
 app.listen(port, () => {
 	console.log(`Runnning on port ${port}`)
 })
+console.log(i); i++;
