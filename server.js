b148a8ef78b0765c2c0ac57411ce92c26a20c1b5
Garrett Long WSL Ubuntu <garrettlong097@gmail.com>
2023-03-26 15:27:45 -0400

    we just can't use alternatives packages....................................................................................?????????????????

--- server.js
+++ server.js
@@ -11,60 +11,50 @@ const port = args.port || 5000
 
 const app = express()
 app.use(express.json())
-//app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}))
 
 
 app.get('/app', (req, res) => {
-	console.log("here1")
 	res.status(200).send("200 OK")
 })
 
 app.get('/app/rps', (req, res) => {
-	console.log("here2")
 	res.status(200).send(JSON.stringify(rps()))
 })
 
 app.get('/app/rpsls', (req, res) => {
-	console.log("here3")
 	res.status(200).send(JSON.stringify(rpsls()))
 })
 
 // JSON params
 app.get('/app/rps/play', (req, res) => {
-	console.log("here4")
 	res.status(200).send(JSON.stringify(rps(req.query.shot)))
 })
 
 app.get('/app/rpsls/play', (req, res) => {
-	console.log("here5")
 	res.status(200).send(JSON.stringify(rpsls(req.query.shot)))
 })
 
 // query params
 app.get('/app/rps/play/:shot', (req, res) => {
-	console.log("here6")
 	res.status(200).send(JSON.stringify(rps(req.body.shot)))
 })
 
 app.get('/app/rpsls/play/:shot', (req, res) => {
-	console.log("here7")
 	res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
 })
 
 // params
 app.get('/app/rps/play/:shot', (req, res) => {
-	console.log("here8")
 	res.status(200).send(JSON.stringify(rps(req.params.shot)))
 })
 
 app.get('/app/rpsls/play/:shot', (req, res) => {
-	console.log("here9")
 	res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
 })
 
 
 app.get('*', (req, res) => {
-	console.log("here10")
 	res.status(404).send('404 NOT FOUND')
 })
 
