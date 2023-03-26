a7bcb5f5f3b86f4750f10f8f94ab802b403a342a
Garrett Long WSL Ubuntu <garrettlong097@gmail.com>
2023-03-26 16:06:41 -0400

    another test23

--- server.js
+++ server.js
@@ -28,26 +28,13 @@ app.get('/app/rpsls', (req, res) => {
 
 // url encoding params
 app.get('/app/rps/play', (req, res) => {
-	res.status(200).send(rps(req.query.shot));
+	var ffs = req.query.shot || req.body.shot
+	res.status(200).send(rps(ffs));
 });
 
 app.get('/app/rpsls/play', (req, res) => {
-	res.status(200).send(rpsls(req.query.shot));
-});
-
-// json params
-app.get('/app/rps/play', (req, res) => {
-	console.log("json body rps");
-	var tst = rps(req.body.shot);
-	console.log(JSON.stringify(tst));
-	res.status(200).send(rps(req.body.shot));
-});
-
-app.get('/app/rpsls/play', (req, res) => {
-	console.log("json body rpsls");
-	var tst = rpsls(req.body.shot);
-	console.log(JSON.stringify(tst));
-	res.status(200).send(tst);
+	var ffs = req.query.shot || req.body.shot
+	res.status(200).send(rpsls(ffs));
 });
 
 // params
